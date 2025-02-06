85f7dd7336008352dac6702f9e1cad2d0b78c2b7
caio-venancio <caio.venancio784@gmail.com>
2025-02-05 23:47:10 -0300

    <build>[PROJECT]:<Segundo teste de Deploy>
    Foi bem sucedido!

--- PROJECT/src/mysite/settings.py
+++ PROJECT/src/mysite/settings.py
@@ -23,7 +23,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
--- PROJECT/src/mysite/wsgi.py
+++ PROJECT/src/mysite/wsgi.py
@@ -6,6 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
+import sys
+print("PYTHON PATH:", sys.path)
+
+try:
+    import django
+    print("Django está instalado. Versão:", django.__version__)
+except ImportError:
+    print("ERRO: Django NÃO está instalado!")
 
 import os
 
